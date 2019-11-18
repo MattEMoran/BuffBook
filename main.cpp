1eb6556f9fcdae967e2a1b37867e7391127bc79c
Matt Moran <mamo2629@colorado.edu>
2019-11-18 23:36:53 +0000

    Added skeleton code

--- main.cpp
+++ main.cpp
@@ -2,6 +2,49 @@
 
 using namespace std;
 
+class BuffBook{
+  private:
+  BuffBook(string filename){
+    generateNetwork(filename);
+  }
+
+  void generateNetwork(string filename){
+
+  }
+
+  void refreshFeed(){
+
+  }
+
+  void addFriend(){
+
+  }
+
+  void removeFriend(){
+
+  }
+
+  void writePost(){
+
+  }
+
+  void listFriends(){
+
+  }
+
+  void listNetwork(){
+
+  }
+
+  void commentOnPost(){
+
+  }
+
+  void likePost(){
+    
+  }
+}
+
 int main() {
-  
+  BuffBook b(Database.txt);
 }
