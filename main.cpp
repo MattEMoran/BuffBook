7e2f2dcf4cb1b8412a3f5d15f6f9dde878bd60d3
Matt Moran <mamo2629@colorado.edu>
2019-11-20 04:44:06 +0000

    Body code and comments

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <stack>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
+
+struct user {
+ string username;
+ string password;
+ vector<post> stream;
+ vector<user> friends;
+ user(string name, string password){
+   username = name;
+   password = password;
+ }
+}
+
+struct post{
+  string title;
+  int likes;
+  string timestamp;
+  vector<string> comments;
+  post(string t, int l, string tm){
+    title = t;
+    l = likes;
+    timestamp = tm;
+  }
+}
 
 class BuffBook{
   private:
@@ -8,40 +34,46 @@ class BuffBook{
     generateNetwork(filename);
   }
 
-  void generateNetwork(string filename){
-
-  }
-
   void refreshFeed(){
-
+    //Display 10 most recent posts from all friends
   }
 
   void addFriend(){
-
+    //Add friend for current user
   }
 
   void removeFriend(){
-
+    //Remove friend from current user's friend list
   }
 
   void writePost(){
-
+    //Create post for current user
   }
 
   void listFriends(){
-
-  }
-
-  void listNetwork(){
-
+    //List current user's friends
   }
 
   void commentOnPost(){
-
+    //Adds comment to post
   }
 
   void likePost(){
-    
+    //Increase likes on particular post
+  }
+
+  //Utility Functions that can be used only by admin//
+
+  void shutDown(){
+    //Save new information to file
+  }
+
+  void listNetwork(){
+    //List all users and their friends
+  }
+
+  void generateNetwork(string filename){
+    //Generate network of users and posts
   }
 }
 
